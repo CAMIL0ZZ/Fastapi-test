ee6079309ab3fca25351adf91a11043ab1a77e18
CAMIL0ZZ <accontreras94@ucatolica.edu.co>
2026-03-12 19:33:54 +0100

    update

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Pokemon(BaseModel):
+    id: int
+    name: str
+    attack: int
+    life: int
+    type: str
+
+
+pokemons = [
+    Pokemon(id=1,name="Bulbasaur",attack=49,life=45,type="grass"),
+    Pokemon(id=4,name="Charmander",attack=52,life=39,type="fire"),
+    Pokemon(id=7,name="Squirtle",attack=48,life=44,type="water"),
+    Pokemon(id=25,name="Pikachu",attack=55,life=35,type="electric"),
+    Pokemon(id=6,name="Charizard",attack=84,life=78,type="fire")
+]
+
+
